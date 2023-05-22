e56487efa562cf85849be66d217c421e62df590c
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-05-22 15:08:39 -0300

    atualização avançado

--- Fibonacci.pas
+++ Fibonacci.pas
@@ -1,15 +1,27 @@
 //exercicio 03 - TIPO FIBONACCI
-n:=0;
-varstr:= '0';//variavel em formato string
-varintg:= StrToFloat(varstr);
-varnum:= NumerictoInt(varintg);
+v1 := 0;
+v3 := 0;
+v2 := 1;
+fibonacci:='';
+fibo_1:='';
+log:= CreateFileLog('Fibonacci.txt');//RELATORIO
 
-BeginPage(PAGE1);
-ClearFields(PAGE1,REC1);
-For j:= 0 to 5 Do Begin
-n:=n+2;
 
-	PAGE1.REC1.CAMPO[j]:= FormatFloat((j+VARNUM)+n,'9999');
-End;
-WriteRecord(PAGE1,REC1);
-EndPage(PAGE1);
+	BeginPage(PAGE1);
+	ClearFields(PAGE1,REC1);
+		For j:= 0 to 9 Do Begin
+			fibonacci:=inttostr(v1);
+			v3:= v1+v2;
+			v1:=v2;
+			v2:=v3;
+			fibo_1:= (fibo_1 + fibonacci+',');
+			//if fibo = ',' then begin
+			//end;
+			PAGE1.REC1.CAMPO[1]:=  fibo_1;
+		End;
+	       WritelnFileLog(log,fibo_1);
+	WriteRecord(PAGE1,REC1);
+	EndPage(PAGE1);
+
+	CloseFileLog(log);
+Convert(1,false,false,false,0,false);
