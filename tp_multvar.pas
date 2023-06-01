bc1b14e008bc2c3dd2ed3733aa50b8b74b0f8c63
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-06-01 11:51:05 -0300

    refactor: contador de linhas
    a função multLIneCount, faz as contagem da linhas da linhas do arquivo

--- tp_multvar.pas
+++ tp_multvar.pas
@@ -7,25 +7,28 @@ linhas    := '';
 linha_pc  := 0;
 fecha     := 0;
 nome      := 'NOME';
-log       := CreateFileLog('parcelas[nome do arquivo].txt');
+log       := CreateFileLog('parcelas.txt');
 	while true do begin
     	
 		//if 1 = se nome for igual ao 'nome' E impri for IGUAL '1' então comece
 		if (nome <> getstring(s,2,0)) AND (impri = 1) then begin
-				WritelnFileLog(log,GetString(multlineItem(linhas,0),2,0)+' - ');
+				WritelnFileLog(log,GetString(multlineItem(linhas,0),2,0)+' - '+FormatFloat(linha_pc,'9')+' PC');
+
 				BeginPage(PAGE1);
 				ClearFields(PAGE1,REC1);
-				    
-					PAGE1.REC1.CAMPO[1]:= GetString(multlineItem(linhas,0),2,0);
-					PAGE1.REC1.CAMPO[2]:= GetString(multlineItem(linhas,0),3,0)+' - '+GetString(multlineItem(linhas,0),4,0);
-					PAGE1.REC1.CAMPO[3]:= GetString(multlineItem(linhas,1),3,0)+' - '+GetString(multlineItem(linhas,1),4,0);
-					PAGE1.REC1.CAMPO[4]:= GetString(multlineItem(linhas,2),3,0)+' - '+GetString(multlineItem(linhas,2),4,0);
-					PAGE1.REC1.CAMPO[5]:= GetString(multlineItem(linhas,3),3,0)+' - '+GetString(multlineItem(linhas,3),4,0);
-	
+					if getstring(s,2,0) <> ' ' then begin  
+							PAGE1.REC1.CAMPO[1]:= GetString(multlineItem(linhas,0),2,0)+' - '+FormatFloat(linha_pc,'####')+' pc';
+							PAGE1.REC1.CAMPO[2]:= GetString(multlineItem(linhas,0),3,0)+' - '+GetString(multlineItem(linhas,0),4,0);
+							PAGE1.REC1.CAMPO[3]:= GetString(multlineItem(linhas,1),3,0)+' - '+GetString(multlineItem(linhas,1),4,0);
+							PAGE1.REC1.CAMPO[4]:= GetString(multlineItem(linhas,2),3,0)+' - '+GetString(multlineItem(linhas,2),4,0);
+							PAGE1.REC1.CAMPO[5]:= GetString(multlineItem(linhas,3),3,0)+' - '+GetString(multlineItem(linhas,3),4,0);
+					end else begin 
+						break
+					end;
 				WriteRecord(PAGE1,REC1);
 				EndPage(PAGE1);
 
-			markup();
+				markup();
 			
 			linhas    := '';
 			linha_pc  := 0;
