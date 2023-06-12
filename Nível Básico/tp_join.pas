a8942a168b9e3ca55474e5f303a2442beb6f282e
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-06-12 11:08:46 -0300

    Update tp_join.pas

--- Nível Básico/tp_join.pas
+++ Nível Básico/tp_join.pas
@@ -1,13 +1,14 @@
 //Execicio 02: tp_join
 
-empresa:='Topdata Processamento de Dados 2021';
+empresa  :='Topdata Processamento de Dados 2021';
+empresa_1:='Topdata Processamento de Dados 2021';
+empresa_2:='Topdata Processamento de Dados 2021';
 
 BeginPage(PAGE1);
 ClearFields(PAGE1,REC1);
     PAGE1.REC1.CAMPO[1]:= subStr(empresa,1,35);
-	PAGE1.REC1.CAMPO[2]:= subStr(empresa,1,30);
-	PAGE1.REC1.CAMPO[3]:= subStr(empresa,32,4);
-	PAGE1.REC1.CAMPO[4]:= subStr(empresa,1,30)+' '+subStr(empresa,32,4);
+	PAGE1.REC1.CAMPO[2]:= subStr(empresa_1,1,30);
+	PAGE1.REC1.CAMPO[3]:= subStr(empresa_2,32,4);
+	PAGE1.REC1.CAMPO[4]:= subStr(empresa_1,1,30)+' '+subStr(empresa_2,32,4);
 WriteRecord(PAGE1,REC1);
 EndPage(PAGE1);
-
