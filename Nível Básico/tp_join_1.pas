cbd3e7010f7f072b75b76eb968ec793057a93b8e
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-06-14 09:37:14 -0300

    atualização Script
    foi adicionada a variavel get numero que busca somente o numero dentro da variavel selecionada

--- Nível Básico/tp_join_1.pas
+++ Nível Básico/tp_join_1.pas
@@ -69,3 +69,38 @@ ClearFields(PAGE1,REC1);
 WriteRecord(PAGE1,REC1);
 EndPage(PAGE1);
 
+//____________________________________________________________________________________________________
+
+//Execicio 02:  tp_join(versão trêS sem variavel externa - ADICIONADO A VARIAVEL GetNumero)
+
+empresa  := 'Topdata Processamento de Dados                                      2021';
+cont     := 0;
+inicio   := 1;
+busca    := GetNumero(empresa,4); {QUE EXTRAI SOMENTE OS NUMEROS DENTRO DA VARIAVEL EMPRESA}
+name     := '';
+for h:= 0 to 99 do begin
+	if subStr(busca,h+1,1) = '' then begin
+	break;
+	end else begin
+	cont:= cont+1;
+	end;
+end;	
+
+for i:= 0 to 999 do begin
+	if substr(empresa,i+1,cont) = busca then begin
+		break;
+	end else begin
+		inicio:=inicio+1;
+	end;
+end;
+
+name:= substr(empresa,inicio,cont);
+
+BeginPage(PAGE1);
+ClearFields(PAGE1,REC1);
+    PAGE1.REC1.CAMPO[1]:= name;
+	//PAGE1.REC1.CAMPO[2]:= empresa_1;
+	//PAGE1.REC1.CAMPO[3]:= empresa_2;
+	//PAGE1.REC1.CAMPO[4]:= empresa_1+' '+empresa_2;
+WriteRecord(PAGE1,REC1);
+EndPage(PAGE1);
