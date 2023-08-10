c82369e0515dd4b983332e182e57cfa800611cca
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-08-10 16:23:09 -0300

    Update tp_registr_05.pas

--- Nível_Avançado/tp_registr_05.pas
+++ Nível_Avançado/tp_registr_05.pas
@@ -2,9 +2,11 @@
 MULTI2 := 2;
 MULTI3 := 3;
 MULTI5 := 5;
-contador_1:= 1;
-contador_2:= 1;
-contador_3:= 1;
+contador_1:= 0;
+contador_2:= 0;
+contador_3:= 0;
+
+//page_1
 	BeginPage(PAGE1);
 	ClearFields(PAGE1,REC1);
 		while contador_1 < 6 do begin
@@ -43,3 +45,41 @@ contador_3:= 1;
 	EndPage(PAGE1);
 markup;
 
+//page2
+	BeginPage(PAGE1);
+	ClearFields(PAGE1,REC1);
+		while contador_1 < 6 do begin
+			if contador_1 > 30 then begin		
+				break;
+			end else begin
+				PAGE1.REC1.CAMPO[contador_1+1]:= FormatFloat((contador_1 * MULTI2),'9');
+			end;
+			contador_1:= contador_1+1;
+		end;		
+	WriteRecord(PAGE1,REC1);
+
+	
+	ClearFields(PAGE1,REC2);
+		while contador_2 < 6 do begin
+			if contador_2 > 30 then begin		
+				break;
+			end else begin
+				PAGE1.REC2.CAMPO[contador_2+1]:= FormatFloat((contador_2 * MULTI3),'9');
+			end;
+			contador_2:= contador_2+1;
+		end;		
+	WriteRecord(PAGE1,REC2);
+	
+
+	ClearFields(PAGE1,REC3);
+		while contador_3 < 6 do begin
+			if contador_3 > 30 then begin		
+				break;
+			end else begin
+				PAGE1.REC3.CAMPO[contador_3+1]:= FormatFloat((contador_3 * MULTI5),'9');
+			end;
+			contador_3:= contador_3+1;
+		end;		
+	WriteRecord(PAGE1,REC3);
+	EndPage(PAGE1);
+markup;	
