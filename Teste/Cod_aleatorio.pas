af7cf017cf7722f01adac10fd8ac5793d1dd19c4
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-08-29 11:29:46 -0300

    Update Cod_aleatorio.pas

--- Teste/Cod_aleatorio.pas
+++ Teste/Cod_aleatorio.pas
@@ -1,22 +1,25 @@
 //exercicio 09 - TP_ALEAT
 letras := 'abcdefghijklmnopqrstuvxz';
 saida:='';
-saida:= substr(letras,random(36)+1,24);
+resposta:='';
+//saida:= substr(letras,random(24)+1,1);
 
 BeginPage(PAGE1);
 ClearFields(PAGE1,REC1);
 	
 	For vertical := 0 to 9 do begin
-		for horizontal := 0 to 30 do begin
-			saida:= saida+''+substr(letras,random(36)+1,24);
+
+		for horizontal := 0 to 29 do begin
+			saida:= saida+' '+substr(letras,random(24)+1,1);
 			PAGE1.REC1.CAMPO[VERTICAL+1]:= saida;
 		end;
-	saida:= '';	
+	saida:='';	
 	end;
-	
+	if InListStr(saida,'M','A','R','C','O','S') Then Begin
+		PAGE1.REC1.CAMPO[11]:= resposta();
+	End;
 	//abort(saida);	
 WriteRecord(PAGE1,REC1);
 EndPage(PAGE1);
-
 
 
