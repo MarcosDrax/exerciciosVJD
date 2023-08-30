fe10b73793c3536ec941c1586fd7d34c051f8662
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-08-30 16:04:17 -0300

    Update Cod_aleatorio.pas

--- Teste/Cod_aleatorio.pas
+++ Teste/Cod_aleatorio.pas
@@ -1,25 +1,35 @@
 //exercicio 09 - TP_ALEAT
 letras := 'abcdefghijklmnopqrstuvxz';
 saida:='';
-resposta:='';
-//saida:= substr(letras,random(24)+1,1);
-
+saida_reser:='';
+cont:= 0;
 BeginPage(PAGE1);
 ClearFields(PAGE1,REC1);
-	
-	For vertical := 0 to 9 do begin
 
+	For vertical := 0 to 9 do begin
+	{ looping vertical com a quantidade de 10 loop's,   }
 		for horizontal := 0 to 29 do begin
-			saida:= saida+' '+substr(letras,random(24)+1,1);
-			PAGE1.REC1.CAMPO[VERTICAL+1]:= saida;
+		{ looping horizontal com a quantidade de 30 loop's, este for esta gerando as letras aleatorias de cada coluna vertical   }
+			saida:= saida+''+substr(letras,random(24)+1,1);
+			PAGE1.REC1.CAMPO[VERTICAL+1]:= saida;			
+			saida_reser:= saida;
 		end;
-	saida:='';	
-	end;
-	if InListStr(saida,'M','A','R','C','O','S') Then Begin
-		PAGE1.REC1.CAMPO[11]:= resposta();
-	End;
-	//abort(saida);	
+
+			{ preciso de uma variavel que reserve a saida antes de fazer o comparativo do IF }
+			saida:='';
+   
+		{ função criada para contagem do valores }
+			for h:=0 to 99999999 do Begin 
+				if substr(saida_reser,h+1,1) = 'm' then begin
+					cont:= cont+1;
+				end else begin
+					break;
+				end;
+			end; 
+    end;			
+
+			{ no if necessito entender  }
+			PAGE1.REC1.TESTE_1[1]:= FormatFloat(cont,'99');
+	
 WriteRecord(PAGE1,REC1);
 EndPage(PAGE1);
-
-
