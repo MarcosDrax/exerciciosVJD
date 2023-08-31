3b8f3c5d865b4c232b3ecadb29576c997b416b82
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-08-31 11:06:51 -0300

    Update Cod_aleatorio.pas

--- Teste/Cod_aleatorio.pas
+++ Teste/Cod_aleatorio.pas
@@ -3,6 +3,8 @@ letras := 'abcdefghijklmnopqrstuvxz';
 saida:='';
 saida_reser:='';
 cont:= 0;
+conti:= 0;
+
 BeginPage(PAGE1);
 ClearFields(PAGE1,REC1);
 
@@ -13,24 +15,28 @@ ClearFields(PAGE1,REC1);
 			saida:= saida+''+substr(letras,random(24)+1,1);
 			PAGE1.REC1.CAMPO[VERTICAL+1]:= saida;			
 			saida_reser:= saida;
+	 
 		end;
 		//abort(saida_reser); 
 			{ preciso de uma variavel que reserve a saida antes de fazer o comparativo do IF }
 			saida:='';
    
 		//função criada para contagem do tamanho da variavel
-			for h:=0 to 99999999 do Begin 
-				if substr(saida_reser,h+1,1) = 'm' then begin
+			for h:=0 to 99 do Begin 
+				if substr(saida_reser,h-1,1) = 'm' then begin
 					cont:= cont+1;
-				end else begin
-					break;
 				end;
-			end; 
-    end;			
+			end;
+			for i:=0 to 99 do Begin 
+				if substr(saida_reser,i-1,1) = 'a' then begin
+					conti:= conti+1;
+				end; 
+			end;			
 			//abort(formatfloat(cont,'9'));
- 			
+		end; 			
+
 			{ no if necessito entender  }
-			PAGE1.REC1.TESTE_1[1]:= FormatFloat(cont,'99');
+			PAGE1.REC1.TESTE_1[1]:= 'posição_1 - '+FormatFloat(conti,'99')+' posição_2 - '+FormatFloat(cont,'99');
 	
 {  bom preciso que o IF faça uma busca de somente 1 letra, dentro da variavel saida, }
 	//abort(formatfloat(cont,'9'));
