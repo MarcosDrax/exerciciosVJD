c21dad32dfe02a34280936b0ac0482c5dd0cb0dc
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-08-31 12:05:19 -0300

    Update Cod_aleatorio.pas

--- Teste/Cod_aleatorio.pas
+++ Teste/Cod_aleatorio.pas
@@ -4,7 +4,10 @@ saida:='';
 saida_reser:='';
 cont:= 0;
 conti:= 0;
-
+cont1:= 0;
+cont2:= 0;
+cont3:= 0;
+cont4:= 0;
 BeginPage(PAGE1);
 ClearFields(PAGE1,REC1);
 
@@ -23,20 +26,35 @@ ClearFields(PAGE1,REC1);
    
 		//função criada para contagem do tamanho da variavel
 			for h:=0 to 99 do Begin 
-				if substr(saida_reser,h-1,1) = 'm' then begin
+				if substr(saida_reser,h+1,1) = 'm' then begin
 					cont:= cont+1;
 				end;
-			end;
-			for i:=0 to 99 do Begin 
-				if substr(saida_reser,i-1,1) = 'a' then begin
+				if substr(saida_reser,h+1,1) = 'm' then begin
+					break;
+				end;
+			end;	
+				{if substr(saida_reser,h-1,1) = 'a' then begin
 					conti:= conti+1;
 				end; 
-			end;			
+				if substr(saida_reser,h-1,1) = 'r' then begin
+					cont1:= cont1+1;
+				end;
+				if substr(saida_reser,h-1,1) = 'c' then begin
+					cont2:= cont2+1;
+				end;
+				if substr(saida_reser,h-1,1) = 'o' then begin
+					cont3:= cont3+1;
+				end;
+				if substr(saida_reser,h-1,1) = 's' then begin
+					cont4:= cont4+1;
+				end;				
+			end;}
+			
 			//abort(formatfloat(cont,'9'));
 		end; 			
 
 			{ no if necessito entender  }
-			PAGE1.REC1.TESTE_1[1]:= 'posição_1 - '+FormatFloat(conti,'99')+' posição_2 - '+FormatFloat(cont,'99');
+			PAGE1.REC1.TESTE_1[1]:= 'posição_1 - '+FormatFloat(cont,'99')+' posição_2 - '+FormatFloat(conti,'99')+' posição_3 - '+FormatFloat(cont1,'99')+' posição_4 - '+FormatFloat(cont2,'99')+' posição_5 - '+FormatFloat(cont3,'99')+' posição_6 - '+FormatFloat(cont4,'99');
 	
 {  bom preciso que o IF faça uma busca de somente 1 letra, dentro da variavel saida, }
 	//abort(formatfloat(cont,'9'));
