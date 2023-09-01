f0e5e68c27e0f463e75ecdb5a5d8bd6c02798a0e
MarcosDrax <131676551+MarcosDrax@users.noreply.github.com>
2023-09-01 17:38:35 -0300

    Update Cod_aleatorio.pas

--- Teste/Cod_aleatorio.pas
+++ Teste/Cod_aleatorio.pas
@@ -7,17 +7,19 @@ letras     := 'abcdefghijklmnopqrstuvxz';
 saida      := '';
 saida_reser:= '';
 palavra    := '';
-inicio     := 0;
+horizontal := 1;
+vertical   := 0;
 cont       := 0;
+qtd_busca  := 0;
 BeginPage(PAGE1);
 ClearFields(PAGE1,REC1);
 
-	For vertical := 0 to 9 do begin
+	For verti := 0 to 9 do begin
 	{ looping vertical com a quantidade de 10 loop's,   }
-		for horizontal := 0 to 29 do begin
+		for horizon := 0 to 29 do begin
 		{ looping horizontal com a quantidade de 30 loop's, este for esta gerando as letras aleatorias de cada coluna vertical   }
 			saida:= saida+''+substr(letras,random(24)+1,1);
-			PAGE1.REC1.CAMPO[VERTICAL+1]:= saida;			
+			PAGE1.REC1.CAMPO[verti+1]:= saida;			
 			saida_reser:= saida;
 	 
 		end;
@@ -31,20 +33,20 @@ ClearFields(PAGE1,REC1);
 					cont:= cont+1;
 					palavra := substr(saida_reser,h+1,1);
 				end;
-	
-			end;				
-			{for h2:=0 to 99 do Begin 			
-				if substr(saida_reser,h2+1,cont) = busca then begin
-				break
-				end else begin
-				inicio:=inicio+1;
-				end;			
-			end;}
-			//abort(palavra);			
-		end; 			
 
+				If qtd_busca = 0 then begin
+					if substr(saida_reser,h+1,1) = BUSCA then begin
+						qtd_busca := 1;
+						break
+					end else begin
+						horizontal:=horizontal+1;
+					end;
+				END;	
+			end;				
+	end;
+			//abort(FormatFloat(horizontal,'999'));
 			{ no if necessito entender  }
-			PAGE1.REC1.TESTE_1[1]:= 'Quantd - '+FormatFloat(cont,'99')+' Letra - '+palavra;
+			PAGE1.REC1.TESTE_1[1]:= 'Quantd - '+FormatFloat(cont,'99')+' / Letra - '+palavra+' / Posição horizontal - '+FormatFloat(horizontal,'99');
 	
 {  bom preciso que o IF faça uma busca de somente 1 letra, dentro da variavel saida, }
 	//abort(formatfloat(cont,'9'));
